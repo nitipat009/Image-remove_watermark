f172e4e713effe517f9810544f48957627482262
nitipat009 <nitipatboongate@gmail.com>
2022-05-02 17:47:18 +0700

    Split Backend!

--- image-watermarkremove/backend/db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-from dotenv import dotenv_values
-config = dotenv_values(".env")
-conn = MongoClient(config.get("MONGODB_URL"))
-db = conn.Toc
--- image-watermarkremove/backend/models/model2.py
+++
@@ -1,9 +0,0 @@
-from lib2to3.pytree import Base
-from fastapi import File
-from pydantic import BaseModel
-
-class Model2(BaseModel) :
-    image_path : bytes  = File(...) 
-    mask_path : bytes = File(...)
-    BATCH_SIZE : int
-    LEARNING_RATE : float 
